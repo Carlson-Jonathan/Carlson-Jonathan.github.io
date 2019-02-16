15184912ab7e7218305bfaaf0955e82d4365316b
Jonathan Carlson <car99020@byui.edu>
2019-02-16 15:02:51 -0600

    adding valentine card

---
+++ script/csstransitions.js
@@ -0,0 +1,16 @@
+/*****************************************************************************
+* This page demonstrates use of HTML 5 tags (audio and video) as well as 
+* the canvas tool. It is becomming increasingly more difficult to come up with
+* creative examples of how to use these topics.
+* Author:
+*     Jonathan Carlson
+* Course: 
+*     CIT 261 section ? Online
+* Instructor:
+*     Brother Eddington
+*****************************************************************************/
+
+"use strict"
+
+
+drawStar(175, 200, 20, 30, 25);
